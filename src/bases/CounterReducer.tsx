3f75836fcfcb8ee72fdca59322b90dd32ecdd9bb
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-29 14:46:54 -0400

    user reducer

---
+++ src/bases/CounterReducer.tsx
@@ -0,0 +1,35 @@
+import { useReducer, useState } from "react";
+
+
+interface CounterState {
+    counter: number;
+    previous: number;
+    changes: number;
+}
+
+const INITIAL_STATE :CounterState = {
+  counter: 0,
+  previous: 0,
+  changes: 0
+}
+
+export const CounterReducer = ({ initialValue = 0 }: Props) => {
+
+  const [state, dispatch] = useReducer(first, INITIAL_STATE)
+  const [ counter, setCounter ] = useState(initialValue);
+
+  const handleClick = () =>{
+    setCounter( prev => prev + 1 )
+  }
+
+  return (
+    <>
+      <h1>Counter Reducer: { counter }</h1>
+      <button onClick={ handleClick }>
+        +1
+      </button>
+    </>
+  )
+}
+
+
